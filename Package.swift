7ca796114784a9cbf42ade81342bb5f841daf5d0
Pavel Kasila <pavel.kasila@gmail.com>
2022-04-09 15:29:57 +0300

    Mąkę CEExtensionKit dynamic

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CEExtensionKit",
+            type: .dynamic,
             targets: ["CEExtensionKit"]),
     ],
     dependencies: [
