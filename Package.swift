e352c9d11d7be6af3545e4b0ca0a79d96106cb95
Kyle <kyle201817146@gmail.com>
2022-10-10 17:49:02 +0800

    Fix CR suggestion

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,8 +13,7 @@ let package = Package(
             targets: ["CodeEditKit"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -27,9 +26,3 @@ let package = Package(
             dependencies: ["CodeEditKit"]),
     ]
 )
-
-#if swift(>=5.6)
-package.dependencies += [
-    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-]
-#endif
