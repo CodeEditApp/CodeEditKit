6305273b201428930f4c5e31ba314736d80d6ca6
Lukas Pistrol <l.pistrol@gmail.com>
2022-10-11 14:36:36 +0200

    Update CEExtensionKitTests.swift

--- Tests/CodeEditKitTests/CEExtensionKitTests.swift
+++ Tests/CodeEditKitTests/CEExtensionKitTests.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import CodeEditKit
 
 final class CEExtensionKitTests: XCTestCase {
-    
+
     // This is only a sample test case
     // until real unit tests are established
     func testExample() throws {
