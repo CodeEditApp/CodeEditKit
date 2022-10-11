bea981a2fd9c81414ce3bbc93c75ef162c5ec1bb
Lukas Pistrol <l.pistrol@gmail.com>
2022-10-11 14:34:03 +0200

    Update CEExtensionKitTests.swift

--- Tests/CodeEditKitTests/CEExtensionKitTests.swift
+++ Tests/CodeEditKitTests/CEExtensionKitTests.swift
@@ -2,6 +2,9 @@ import XCTest
 @testable import CodeEditKit
 
 final class CEExtensionKitTests: XCTestCase {
+    
+    // This is only a sample test case
+    // until real unit tests are established
     func testExample() throws {
         XCTAssertTrue(true)
     }
