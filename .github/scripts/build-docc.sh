8e7cf6dc55acc1a43cf37bbc25faa5ab33b0239b
Lukas Pistrol <lukas@pistrol.com>
2022-11-12 22:43:52 +0100

    use xcodebuild docbuild command instead of swift-docc-plugin

---
+++ .github/scripts/build-docc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export LC_CTYPE=en_US.UTF-8
+
+set -o pipefail && xcodebuild clean docbuild -scheme CodeEditKit \
+    -destination generic/platform=macos \
+    OTHER_DOCC_FLAGS="--transform-for-static-hosting --hosting-base-path CodeEditKit --output-path ./docs" | xcpretty
