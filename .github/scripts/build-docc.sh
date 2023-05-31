d636ec98319751e4a2443604c021ea66e2a8569f
Wouter Hennen <62355975+Wouter01@users.noreply.github.com>
2023-05-31 21:09:08 +0200

    Update build-docc.sh

--- .github/scripts/build-docc.sh
+++ .github/scripts/build-docc.sh
@@ -4,4 +4,5 @@ export LC_CTYPE=en_US.UTF-8
 
 set -o pipefail && xcodebuild clean docbuild -scheme CodeEditKit \
     -destination generic/platform=macos \
+    -skipPackagePluginValidation \
     OTHER_DOCC_FLAGS="--transform-for-static-hosting --hosting-base-path CodeEditKit --output-path ./docs" | xcpretty
